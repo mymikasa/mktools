93828f75b0b257bc3f22e4622231c5976f993dc3
zangmengjie <zangmengjie@idstaff.com>
2023-06-13 19:28:38 +0800

    fix: init pool

---
+++ pool/deque.go
@@ -0,0 +1,209 @@
+package pool
+
+import "fmt"
+
+const minCapacity = 16
+
+type Deque[T any] struct {
+	buf    []T
+	head   int
+	tail   int
+	count  int
+	minCap int
+}
+
+func New[T any](size ...int) *Deque[T] {
+	var capacity, minimum int
+	if len(size) >= 1 {
+		capacity = size[0]
+		if len(size) >= 2 {
+			minimum = size[1]
+		}
+	}
+
+	minCap := minCapacity
+	for minCap < minimum {
+		minCap <<= 1
+	}
+
+	var buf []T
+	if capacity != 0 {
+		bufSize := minCap
+		for bufSize < capacity {
+			bufSize <<= 1
+		}
+
+		buf = make([]T, bufSize)
+	}
+
+	return &Deque[T]{
+		buf:    buf,
+		minCap: minCap,
+	}
+}
+
+// Cap returns the current capacity of the Deque.
+func (q *Deque[T]) Cap() int {
+	if q == nil {
+		return 0
+	}
+	return len(q.buf)
+}
+
+// Len returns the number of elements currently stored in the queue.
+func (q *Deque[T]) Len() int {
+	if q == nil {
+		return 0
+	}
+	return q.count
+}
+
+func (q *Deque[T]) PushBack(elem T) {
+	q.grow()
+
+	q.buf[q.tail] = elem
+	q.tail = q.next(q.tail)
+	q.count++
+}
+
+func (q *Deque[T]) PushFront(elem T) {
+	q.grow()
+
+	// Calculate new head position.
+	q.head = q.prev(q.head)
+	q.buf[q.head] = elem
+	q.count++
+}
+
+func (q *Deque[T]) PopFront() T {
+	if q.count < 0 {
+		panic("deque: PopFront() called on empty queue")
+	}
+	ret := q.buf[q.head]
+
+	var zero T
+	q.buf[q.head] = zero
+	q.head = q.next(q.head)
+	q.count--
+
+	q.shrinkIfExcess()
+	return ret
+}
+
+func (q *Deque[T]) Front() T {
+	if q.count <= 0 {
+		panic("deque: Front() called when empty")
+	}
+	return q.buf[q.head]
+}
+
+func (q *Deque[T]) Back() T {
+	if q.count <= 0 {
+		panic("deque: Back() called when empty")
+	}
+	return q.buf[q.prev(q.tail)]
+}
+
+func (q *Deque[T]) At(i int) T {
+	if i < 0 || i > q.count {
+		panic(outOfRangeText(i, q.Len()))
+	}
+
+	return q.buf[(q.head+i)&(len(q.buf)-1)]
+}
+
+func (q *Deque[T]) Set(i int, elem T) {
+	if i < 0 || i > q.count {
+		panic(outOfRangeText(i, q.Len()))
+	}
+	q.buf[(q.head+i)&(len(q.buf)-1)] = elem
+}
+
+func (q *Deque[T]) Index(f func(T) bool) int {
+	if q.Len() > 0 {
+		modBits := len(q.buf) - 1
+		for i := 0; i < q.count; i++ {
+			if f(q.buf[(q.head+i)&modBits]) {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+func (q *Deque[T]) RIndex(f func(T) bool) int {
+	if q.Len() > 0 {
+		modBits := len(q.buf) - 1
+		for i := q.count - 1; i >= 0; i-- {
+			if f(q.buf[(q.head+i)&modBits]) {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+func (q *Deque[T]) Clear() {
+	var zero T
+
+	modBits := len(q.buf) - 1
+	h := q.head
+	for i := 0; i < q.Len(); i++ {
+		q.buf[(h+i)&modBits] = zero
+	}
+
+	q.head = 0
+	q.tail = 0
+	q.count = 0
+}
+
+func outOfRangeText(i int, length int) any {
+	return fmt.Sprintf("deque: index out of range %d with length %d", i, len)
+}
+
+// grow resizes up if the buffer is full.
+func (q *Deque[T]) grow() {
+	if q.count != len(q.buf) {
+		return
+	}
+	if len(q.buf) == 0 {
+		if q.minCap == 0 {
+			q.minCap = minCapacity
+		}
+		q.buf = make([]T, q.minCap)
+		return
+	}
+	q.resize()
+}
+
+// next returns the next buffer position wrapping around buffer
+func (q *Deque[T]) next(tail int) int {
+	return (tail + 1) & (len(q.buf) - 1) // bitwise modulus
+}
+
+// prev returns the previous buffer position wrapping around buffer.
+func (q *Deque[T]) prev(head int) int {
+	return (head - 1) & (len(q.buf) - 1)
+}
+
+// shrinkIfExcess resize down if the buffer 1/4 full.
+func (q *Deque[T]) shrinkIfExcess() {
+	if len(q.buf) > q.minCap && (q.count<<2) == len(q.buf) {
+		q.resize()
+	}
+}
+
+// resize resizes the deque to fix
+func (q *Deque[T]) resize() {
+	newBuf := make([]T, q.count<<1)
+	if q.tail > q.head {
+		copy(newBuf, q.buf[q.head:q.tail])
+	} else {
+		n := copy(newBuf, q.buf[q.head:])
+		copy(newBuf[n:], q.buf[:q.tail])
+	}
+
+	q.head = 0
+	q.tail = q.count
+	q.buf = newBuf
+}
